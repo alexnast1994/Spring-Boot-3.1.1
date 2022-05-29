b3ba8b921dce352371b568c75e82be0938b2fba8
alexnast <flashwashcar@yandex.ru>
2022-05-29 18:01:37 +0700

    boot 1.0

---
+++ src/main/java/web/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package web.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.model.User;
+import web.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceTest {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceTest(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
